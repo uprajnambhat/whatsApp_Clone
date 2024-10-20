cbc52489d18735e187bc209e6fd4678be672272c
uprajnambhat <uprajnambhat96@gmail.com>
2024-10-20 20:54:21 +0530

    updated the basename and url inpackage.json

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/chatApp">
+    <BrowserRouter basename="/whatsApp_Clone">
       <Provider store={store}>
         <App />
       </Provider>
